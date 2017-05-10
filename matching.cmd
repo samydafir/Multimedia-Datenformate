8294b8ce2a4d623c2c84a716e52aaaa478ba0bf9
samydafir <samydafir@yahoo.de>
2017-05-10 19:31:33 +0200

    added matching.cmd. see readme

---
+++ matching.cmd
@@ -0,0 +1,6 @@
+@ECHO OFF
+SET startfolder=experiments\images
+for /D %%f IN (%startfolder%\output\*) do (
+    start matlab -nosplash -nodisplay -nodesktop -r "automateMatcher('%startfolder%\original','%%f','Settings\settingsMaxCurvature.ini');exit;"
+    ping -n 60 127.0.0.1 > NUL
+ )
